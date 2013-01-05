4c3340b8d1365fa5a1e92963efc9e7ba3894760b
vsethi13 <vsethi13@2e613de2-a5d1-dc81-bfd7-cc5aacbdac82>
2013-01-05 18:45:19 +0000

    check boxes issue pending

--- trunk/WorkflowManagement_01/src/action/EditorStageAssign.java
+++ trunk/WorkflowManagement_01/src/action/EditorStageAssign.java
@@ -1,21 +1,44 @@
 package action;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import model.StageDetails;
+import model.UserDetails;
 import model.UserRole;
 import model.WorkflowDetails;
 
 public class EditorStageAssign {
 	private int stageID, workflowID;
-	ArrayList<UserRole> usrlist;
+	private ArrayList<UserDetails> usrlist = new ArrayList<UserDetails>();
 	private List<WorkflowDetails> stageList;
-	private Map<Integer, Boolean> checkboxes;
+	private List<String> checkboxes = null;
 	
 	public String execute(){
 		this.setStageList(WorkflowDetails.find(this.getWorkflowID(), ""));
-		usrlist = UserRole.find("WHERE w_id='" + workflowID + "' AND role IN ('author', 'publisher')");
+		usrlist = UserDetails.find("AND w_id='" + workflowID + "' AND role IN ('author', 'publisher')");
+//		System.out.println(usrlist.get(0).getUser_role().getUser_id());
+		return "assign_stage_continue";
+	}
+	
+	public String assignUsers(){
+		int iSize;
+		this.setStageList(WorkflowDetails.find(this.getWorkflowID(), ""));
+		usrlist = UserDetails.find("AND w_id='" + workflowID + "' AND role IN ('author', 'publisher')");
+//		usrlist = UserRole.find("WHERE w_id='" + workflowID + "' AND role IN ('author', 'publisher')");
+		if(checkboxes == null)
+			iSize = 0;
+		else
+			iSize = checkboxes.size();
+		System.out.println(iSize);
+        for (int i = 0; i < iSize; i++) {
+            System.out.println("checked item #" + i + " -> " + checkboxes.get(i));
+            StageDetails.insert(workflowID, stageID, checkboxes.get(i));
+         }
+        
+        this.setCheckboxes(this.getCheckboxes());
 		return "assign_stage_continue";
 	}
 
@@ -35,14 +58,6 @@ public class EditorStageAssign {
 		this.workflowID = workflowID;
 	}
 
-	public ArrayList<UserRole> getUsrlist() {
-		return usrlist;
-	}
-
-	public void setUsrlist(ArrayList<UserRole> usrlist) {
-		this.usrlist = usrlist;
-	}
-
 	public List<WorkflowDetails> getStageList() {
 		return stageList;
 	}
@@ -51,11 +66,20 @@ public class EditorStageAssign {
 		this.stageList = stageList;
 	}
 
-	public Map<Integer, Boolean> getCheckboxes() {
+	public List<String> getCheckboxes() {
 		return checkboxes;
 	}
 
-	public void setCheckboxes(Map<Integer, Boolean> checkboxes) {
+	public void setCheckboxes(List<String> checkboxes) {
 		this.checkboxes = checkboxes;
 	}
+
+	public ArrayList<UserDetails> getUsrlist() {
+		return usrlist;
+	}
+
+	public void setUsrlist(ArrayList<UserDetails> usrlist) {
+		this.usrlist = usrlist;
+	}
+
 }
